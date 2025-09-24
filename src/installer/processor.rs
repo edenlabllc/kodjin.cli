0719b23c9b2462cd1cb02b5f2ca9f0719fc4857c
Ilya Zlobintsev <ilya.zl@protonmail.com>
2025-09-24 09:27:17 +0300

    feat: print uninstall on local dir

--- src/installer/processor.rs
+++ src/installer/processor.rs
@@ -191,6 +191,9 @@ async fn process_resources_type(
             Action::Uninstall => match existing {
                 Some(existing) => match ctx.fhir_client.delete(resource_type, &existing.id).await {
                     Ok(()) => {
+                        bar.suspend(|| {
+                            println!("Deleted {resource_type} {}", style(existing.id).bold());
+                        });
                         processed_count += 1;
                         current_progress.lock().unwrap().report.removed += 1;
                     }
